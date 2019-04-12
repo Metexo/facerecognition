94de9872580ab4ec2ceb016400a9c055c82fe5db
Metexo <13kubek@gmail.com>
2019-04-12 15:37:12 +0200

    Moved API key to backend, started validation

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: ''
- });
+
  
 
 const pariclesOptions ={
@@ -106,10 +103,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-    .predict(
-       Clarifai.FACE_DETECT_MODEL,
-       this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method:'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
     .then(response => {
       if(response){
         fetch('http://localhost:3000/image', {
