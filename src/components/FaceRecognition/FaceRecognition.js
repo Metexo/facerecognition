32f37a411afdc5ae363b632c0b35af8226d15590
Metexo <13kubek@gmail.com>
2019-04-24 14:35:35 +0200

    Fixed problem with boxes around faces

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import './FaceRecognition.css'
 
-const createDiv = (boxes) => {
-    const image = document.getElementById('imageBox');
-    for(const box in boxes) {
-        let div = document.createElement("div");
-        div.classList.add('bounding-box');
-        div.style.top = boxes[box].topRow +"px";
-        div.style.right = boxes[box].rightCol+"px";
-        div.style.bottom = boxes[box].bottomRow+"px";
-        div.style.left = boxes[box].leftCol+"px";
-        image.appendChild(div);
-    }
-}
 
-const FaceRecognition = ({imageUrl, box}) => {
-    return(
-        <div className='center ma'>
-            <div id="imageBox" className ='absolute mt2'>
-                <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-                {createDiv(box)}
+class FaceRecognition extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+    
+    createDiv = (boxes) => {
+        const image = document.getElementById('imageBox');
+        const boudingBox = document.getElementsByClassName('bounding-box')
+
+
+        if(boudingBox.length === 0){
+            for(const box in boxes) {
+                    let div = document.createElement("div");
+                    div.classList.add('bounding-box');
+                    div.style.top = boxes[box].topRow +"px";
+                    div.style.right = boxes[box].rightCol+"px";
+                    div.style.bottom = boxes[box].bottomRow+"px";
+                    div.style.left = boxes[box].leftCol+"px";
+                    image.appendChild(div);
+                
+            }
+        }
+    }
+
+    deleteBoxes = () => {
+        const boudingBox = document.getElementsByClassName('bounding-box');
+        console.log(boudingBox);
+        while(boudingBox.length>0){
+            boudingBox[0].parentNode.removeChild(boudingBox[0]);
+        }
+    }
+
+    render(){
+        return(
+            
+            <div className='center ma'>
+                <div id="imageBox" className ='absolute mt2'>
+                    <img id='inputimage' alt='' src={this.props.imageUrl} width='500px' heigh='auto'/>
+                    {this.deleteBoxes()}
+                    {this.createDiv(this.props.box)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default FaceRecognition;
